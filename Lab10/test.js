6e158c5023efd10f40b8e78d54d19ba5b033c515
Russel P <70307796+RPearson808@users.noreply.github.com>
2020-10-14 23:35:12 -1000

    Finished Lab10

--- Lab10/test.js
+++ Lab10/test.js
@@ -1,8 +1,27 @@
+var prod_quant = [30, 9, 4, 30, 15];
 var product1 = {
     "item": "Blue Apron",
-    "quantity": 30,
+    "quantity": prod_quant[0],
     "price": 15.78,
     "SKU#": "1234"
 }
-delete product1["SKU#"];
-console.log(product1);
+//Product 2
+var item2 = "Red Utensil Set";
+var quantity2 = prod_quant[1];
+var price2 = 23.09;
+//Product 3
+var item3 = "Orange Mixing Bowl";
+var quantity3 = prod_quant[2];
+var price3 = 8.64;
+//Product 4
+var item4 = "Purple Chefs Hat";
+var quantity4 = prod_quant[3];
+var price4 = 18.72;
+//Product 5
+var item5 = "Green Spatula";
+var quantity5 = prod_quant[4];
+var price5 = 3.45;
+
+for (i=0; i < prod_quant.length; i++) {
+    console.log(prod_quant[i]);
+}
