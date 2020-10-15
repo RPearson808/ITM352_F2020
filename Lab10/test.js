e83242f14cb368be3bebe7d287a97fc0fca561d4
Russel P <70307796+RPearson808@users.noreply.github.com>
2020-10-14 22:58:40 -1000

    Worked more on Lab10

--- Lab10/test.js
+++ Lab10/test.js
@@ -2,6 +2,7 @@ var product1 = {
     "item": "Blue Apron",
     "quantity": 30,
     "price": 15.78,
+    "SKU#": "1234"
 }
-product1.push("SKU#": 1234);
+delete product1["SKU#"];
 console.log(product1);
