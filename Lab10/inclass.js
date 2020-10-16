542593ac27f4ad2276129a34844dc5ff4cbd0225
Russel P <70307796+RPearson808@users.noreply.github.com>
2020-10-16 12:24:55 -1000

    New inclass loops

--- Lab10/inclass.js
+++ Lab10/inclass.js
@@ -1,12 +1,17 @@
-var employees = ["Celopatra", "Gandhi", "Joan of Arc", "Abraham Lincoln", "JFK", "Jesus"];
+var employees = ["Celopatra", "Gandhi", "Joan of Arc", "Abraham Lincoln", "JFK", "Jesus Christo"];
 var my_kids = [["Kirk", 6, 59.5], ["Spock", 2, 35.2], ["McCoy", 19, 200]];
 console.log(my_kids);
-
+/*
 var my_kid1 = {
     "name": "Kirk",
     "age": 6,
     "weight": 59.5
 };
+
+for (attrib in my_kid1) {
+    console.log(my_kid1[attrib]); //[attrib] becomes the var within object my_kid1
+    console.log(my_kid1.attrib); //.attrib is a secondary var name within object my_kid1
+}
 
 var my_kid2 = {
     "name": "Spock",
@@ -28,16 +33,23 @@ for (i=0; i < fav_kids.length; i++) {
     console.log("My kid " + fav_kids[i].name + ` is ${fav_kids[i].age} years old `);
 }
 
-/*
+
 for (i=0; i < my_kids.length; i++){
     console.log("My kid %s weighs %d pounds.", my_kids[i][0], my_kids[i][2]);
 }
-/*
-(for (i=0; i < employees.length; i++) {
+*/
+for (i=0; i < employees.length; i++) {
     if (i%2 == 0) {
         console.log("Team 1 member: " + employees[i]);
     } else {
         console.log("Team 2 member: " + employees[i]);
     }
 }
-*/
+
+for (empl of employees){
+    if (i%2 == 0) {
+        console.log("Team 1 member: " + employees[i]);
+    } else {
+        console.log("Team 2 member: " + employees[i]);
+    }
+}
