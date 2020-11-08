b47799892061e2fedd467443c049d86839a0db60
Russel P <70307796+RPearson808@users.noreply.github.com>
2020-11-07 14:59:56 -1000

    Finally figured out why I kept getting [object Object]
    I troubleshot too hard and overthought the process

---
+++ Pearson_Russel_Assignment1/public/products.js
@@ -0,0 +1,68 @@
+var products_array = [    
+    {
+        name: "Black Lotus",
+        price: 89999.99,
+        quantity: 1,
+        img: "../images/a_black_lotus.jpg"
+    },
+    
+    {
+        name: "Mox Pearl",
+        price: 7999.99,
+        quantity: 5,
+        img:"../images/a_mox_pearl.jpg"
+    },
+       
+    {
+        name: "Mox Sapphire",
+        price: 14999.99,
+        quantity: 2,
+        img:"../images/a_mox_sapphire.jpg"
+    },
+
+    {
+        name: "Mox Jet",
+        price: 8799.99,
+        quantity: 4,
+        img:"../images/a_mox_jet.jpg"
+    },
+
+    {
+        name: "Mox Ruby",
+        price: 9999.99,
+        quantity: 3,
+        img:"../images/a_mox_ruby.jpg"
+    },
+ 
+    {
+        name: "Mox Emerald",
+        price: 6599.99,
+        quantity: 20,
+        img:"../images/a_mox_emerald.jpg"
+    },
+
+    {
+        name: "Ancestral Recall",
+        price: 9599.99,
+        quantity: 3,
+        img:"../images/a_ancestral_recall.jpg"
+    },
+
+    {
+        name: "Timetwister",
+        price: 10000.99,
+        quantity: 2,
+        img:"../images/a_timetwister.jpg"
+    },
+    
+    {
+        name: "Time Walk",
+        price: 8399.99,
+        quantity: 4,
+        img:"../images/a_time_walk.jpg"
+    }
+];
+
+if(typeof module != 'undefined') {
+    module.exports.products_array = products_array;
+  }
---
+++ Pearson_Russel_Assignment1/server.js
@@ -0,0 +1,32 @@
+// product initialization
+var product_data = require('./public/products.js');
+var products= product_data.products_array;
+var maxQuantity = 0;
+// using a for loop to dynamically find out total quantity of products
+for (i=0; i < product_data.length; i++) {
+    maxQuantity += product_data[i].quantity;
+}
+// webpage modules
+var express = require('express');
+var expressApp = express();
+var bodyParser = require("body-parser");
+var fs = require('fs');
+
+// telling express what folder to serve
+expressApp.use(express.static('./public'));
+expressApp.use( bodyParser.urlencoded({ extended: true }) );
+
+// bringing over isNonNegInt from Lab12 w/ some changes
+function isNonNegInt(stringToCheck, returnErrors = false) {
+    errors = []; // Assume no errors at first
+    // First check if string is a number value
+    if (Number(stringToCheck) != stringToCheck) errors.push('Please enter a valid number.');
+    // Then check if it is non-negative
+    if (stringToCheck < 0) errors.push('Please enter a non-negative amount.');
+    // Finally check that it is an integer and a number value
+    if (typeof stringToCheck == Number && parseInt(stringToCheck) != stringToCheck) errors.push('Please enter an amount without any decimals.');
+
+    return returnErrors ? errors : (errors.length == 0);
+}
+
+expressApp.listen(8080, () => console.log(`listening on port 8080`));
