7cc387af3db4616a00d2c5e917ec1b7a68be3b3f
Russel P <70307796+RPearson808@users.noreply.github.com>
2020-12-08 20:47:39 -1000

    server.js and invoiceGen.js

--- Pearson_Russel_Assignment3/server.js
+++ Pearson_Russel_Assignment3/server.js
@@ -10,3 +10,34 @@ var userDatabase = 'userData.json';
 // telling express what folder to serve
 expressApp.use(express.static('./public'));
 expressApp.use( bodyParser.urlencoded({ extended: true }) );
+
+if (fs.existsSync(userDatabase)) {
+    var raw_data = fs.readFileSync(userDatabase, 'utf-8');
+    var user_data = JSON.parse(raw_data);
+} else {
+    console.log("ERROR: Unable to read file " + userDatabase);
+    exit();
+}
+
+expressApp.all('*', function (request, response, next) {
+    console.log(request.method + ' to ' + request.path);
+    next();
+});
+
+expressApp.get("/login", function (request, response) {
+
+});
+
+expressApp.post("/login", function (request, response) {
+
+});
+
+expressApp.get("/register", function (request, response) {
+
+ });
+
+ expressApp.post("/register", function (request, response) {
+
+ });
+
+expressApp.listen(8080, () => console.log(`listening on port 8080`));
