0a90b7171f9171bbc2c96f68aea5560454768e55
Russel P <70307796+RPearson808@users.noreply.github.com>
2020-12-09 17:07:17 -1000

    Changed how server.js and index.html display products

--- Pearson_Russel_Assignment3/server.js
+++ Pearson_Russel_Assignment3/server.js
@@ -1,16 +1,24 @@
-// webpage modules
+/*
+----- Author: Russel Pearson
+----- Description: The main server application of my website; handles
+      login information, registering users, and invoice generation
+*/
+// webpage module initialization
 var express = require('express');
 var expressApp = express();
 var bodyParser = require("body-parser");
 var fs = require('fs');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
-// init for user data
+// initialize user database
 var userDatabase = 'userData.json';
 
 // telling express what folder to serve
 expressApp.use(express.static('./public'));
-expressApp.use( bodyParser.urlencoded({ extended: true }) );
+expressApp.use(bodyParser.urlencoded({ extended: true }));
 
+// allowing userData.json to be read and written by the server
 if (fs.existsSync(userDatabase)) {
     var raw_data = fs.readFileSync(userDatabase, 'utf-8');
     var user_data = JSON.parse(raw_data);
@@ -18,6 +26,10 @@ if (fs.existsSync(userDatabase)) {
     console.log("ERROR: Unable to read file " + userDatabase);
     exit();
 }
+
+// allowing express to use sessions and cookies
+expressApp.use(session({secret: "ITM352 rocks!"}));
+expressApp.use(cookieParser());
 
 expressApp.all('*', function (request, response, next) {
     console.log(request.method + ' to ' + request.path);
@@ -34,10 +46,14 @@ expressApp.post("/login", function (request, response) {
 
 expressApp.get("/register", function (request, response) {
 
- });
+});
 
- expressApp.post("/register", function (request, response) {
+expressApp.post("/register", function (request, response) {
 
- });
+});
+
+expressApp.post("/process_form", function (request, response) {
+
+});
 
 expressApp.listen(8080, () => console.log(`listening on port 8080`));
