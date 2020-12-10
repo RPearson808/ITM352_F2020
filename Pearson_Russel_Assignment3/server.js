78821eb22fe656af4ad49e8c468f24eb5c71dc3a
RPearson808 <pearsonr@hawaii.edu>
2020-12-10 13:05:27 -1000

    Added login.html and register.html

--- Pearson_Russel_Assignment3/server.js
+++ Pearson_Russel_Assignment3/server.js
@@ -22,13 +22,16 @@ expressApp.use(bodyParser.urlencoded({ extended: true }));
 if (fs.existsSync(userDatabase)) {
     var raw_data = fs.readFileSync(userDatabase, 'utf-8');
     var user_data = JSON.parse(raw_data);
+    console.log(user_data);
 } else {
     console.log("ERROR: Unable to read file " + userDatabase);
     exit();
-}
+ }
 
 // allowing express to use sessions and cookies
-expressApp.use(session({secret: "ITM352 rocks!"}));
+expressApp.use(session(
+    { secret: "ITM352 rocks!" }
+));
 expressApp.use(cookieParser());
 
 expressApp.all('*', function (request, response, next) {
@@ -37,11 +40,22 @@ expressApp.all('*', function (request, response, next) {
 });
 
 expressApp.get("/login", function (request, response) {
-
+    contents = fs.readFileSync('./public/login.html', 'utf-8');
+    response.send(contents);
 });
 
 expressApp.post("/login", function (request, response) {
-
+    POST = request.body;
+    console.log(POST); // for checking internally if POST is being read correctly.
+    username_from_form = POST['username'].toLowerCase();
+    password_from_form = POST['password'];
+    if (username_from_form == user_data[username_from_form]['auth'] && password_from_form == user_data[username_from_form]['password']) {
+        str = `${username_from_form} = ${user_data}['${username_from_form}']['username']`;
+        response.send(str);
+    } else {
+        str = `${username_from_form} is not the same as ${user_data}[${username_from_form}]['username']`;
+        response.send(str);
+    }
 });
 
 expressApp.get("/register", function (request, response) {
